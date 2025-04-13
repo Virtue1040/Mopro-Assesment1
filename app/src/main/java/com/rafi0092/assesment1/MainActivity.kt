f03c8bc493dfa42d6f4c921e728242b2d0009c88
Virtue1040 <rafi.hidayat1313@gmail.com>
2025-04-14 04:46:34 +0700

    Tambah 4 input (2 dropdown dan 2 input) file size dan download speed untuk inputnya

--- app/src/main/java/com/rafi0092/assesment1/MainActivity.kt
+++ app/src/main/java/com/rafi0092/assesment1/MainActivity.kt
@@ -6,30 +6,44 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.input.TextFieldLineLimits
+import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.rafi0092.assesment1.navigation.Screen
@@ -87,8 +101,159 @@ fun MainScreen(navController: NavHostController) {
 }
 
 @Composable
-fun ScreenContent(modifier: Modifier, navController: NavHostController) {
+fun IconPicker(isError: Boolean, unit: String) {
+    if (isError) {
+        Icon(imageVector = Icons.Filled.Warning, contentDescription = null)
+    } else {
+        Text(
+            text = unit
+        )
+    }
+}
 
+@Composable
+fun ErrorHint(isError: Boolean) {
+    if (isError) {
+        Text(
+            text = stringResource(R.string.input_invalid)
+        )
+    }
+}
+
+@Composable
+fun ScreenContent(modifier: Modifier, navController: NavHostController) {
+    val choiceUkuranFile = listOf(
+        "bytes (B)" ,
+        "kilobytes (KB)",
+        "megabytes (MB)",
+        "gigabytes (GB)",
+        "terabytes (TB)",
+        "petabytes (PB)",
+        "exabytes (EB)",
+        "zettabytes (ZB)",
+        "yottabytes (YB)",
+        "bits (b)",
+        "kilobits (Kb)",
+        "megabits (Mb)",
+        "gigabits (Gb)",
+        "terabits (Tb)",
+    )
+
+    var ukuranFile by remember { mutableStateOf("") }
+    var kecepatanInternet by remember { mutableStateOf("") }
+
+    var ukuranFileError by rememberSaveable { mutableStateOf(false) }
+    var kecepatanInternetError by rememberSaveable { mutableStateOf(false) }
+
+    var satuanUkuranFile by remember { mutableStateOf(choiceUkuranFile[0]) }
+    var satuanKecepatanInternet by remember { mutableStateOf(choiceUkuranFile[0]) }
+
+    Column(
+        modifier = modifier.fillMaxSize().verticalScroll(rememberScrollState()).padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(id = R.string.intro),
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Dropdown(satuanUkuranFile, {
+            satuanUkuranFile = it
+        })
+
+        OutlinedTextField(
+            value = ukuranFile,
+            onValueChange = { ukuranFile = it },
+            label = {
+                Text(
+                    text = stringResource(R.string.file_size)
+                )
+            },
+            trailingIcon = {
+                IconPicker(ukuranFileError, satuanUkuranFile.substringAfter("(").substringBefore(")") )
+            },
+            supportingText = {
+                ErrorHint(ukuranFileError)
+            },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            modifier = Modifier.fillMaxWidth().padding()
+        )
+
+        Dropdown(satuanKecepatanInternet, {
+            satuanKecepatanInternet = it
+        })
+
+        OutlinedTextField(
+            value = kecepatanInternet,
+            onValueChange = { kecepatanInternet = it },
+            label = {
+                Text(
+                    text = stringResource(R.string.download_Speed)
+                )
+            },
+            trailingIcon = {
+                IconPicker(kecepatanInternetError, satuanKecepatanInternet.substringAfter("(").substringBefore(")"))
+            },
+            supportingText = {
+                ErrorHint(kecepatanInternetError)
+            },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Dropdown(satuanUkuranFile : String, onValueChange : (String) -> Unit = {}, choiceUkuranFile : List<String> = listOf()) {
+
+    var expanded by remember { mutableStateOf(false) }
+
+    ExposedDropdownMenuBox(
+        expanded = expanded,
+        onExpandedChange = { },
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        TextField(
+            value = satuanUkuranFile,
+            onValueChange = { },
+            label = { stringResource(R.string.file_size_type) },
+            modifier = Modifier.fillMaxWidth(),
+            readOnly = true,
+            colors = ExposedDropdownMenuDefaults.textFieldColors(),
+            trailingIcon = {
+                ExposedDropdownMenuDefaults.TrailingIcon(
+                    expanded = expanded
+                )
+            },
+        )
+        ExposedDropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            choiceUkuranFile.forEach { option ->
+                DropdownMenuItem(
+                    text = { Text(text = option) },
+                    onClick = {
+                        onValueChange(option)
+                        expanded = false
+                    },
+                    contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding
+                )
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
